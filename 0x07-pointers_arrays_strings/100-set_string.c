7db6925a2d61bf28f6f4ea332d22a06760fe7be4
Williams Osimen <greycodes@protonmail.com>
2024-07-08 21:43:48 -0700

    double pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - function to set value of a pointer to char.
+ * @s: double pointer to address
+ * @to: the original pointer
+ *
+ * Return: chessboard.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
